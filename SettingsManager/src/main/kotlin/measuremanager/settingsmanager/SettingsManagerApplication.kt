2e1adcd2566ad2d99653ed0bff05fdb6bb40981a
R3M1d4Cod3r <superchristian9@gmail.com>
2025-05-23 11:12:05 +0200

    init

---
+++ SettingsManager/src/main/kotlin/measuremanager/settingsmanager/SettingsManagerApplication.kt
@@ -0,0 +1,11 @@
+package measuremanager.settingsmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SettingsManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SettingsManagerApplication>(*args)
+}
